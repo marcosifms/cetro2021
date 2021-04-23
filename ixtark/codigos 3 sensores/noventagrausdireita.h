3830e730efa7c9069ac9dd8630f7d57a7dc57a30
marcosifms <65918945+marcosifms@users.noreply.github.com>
2021-04-23 17:33:15 -0400

    Correção de sintaxe
    Mudado o nome do calibrador para minúsculo.
    Foi organizado o código do principal, noventaesquerda e noventadireita.

--- ixtark/codigos 3 sensores/noventagrausdireita.h
+++ ixtark/codigos 3 sensores/noventagrausdireita.h
@@ -1,36 +1,36 @@
-void noventagrausdireita(){
+void noventagrausdireita()
+{
 
 	//90 graus direita
-	if(cor2!=BRANCO){
+	if (cor2 != BRANCO)
+	{
 		stopAllMotors();
-		if(cor4!=BRANCO){
-			wait(100,milliseconds);
+		if (cor4 != BRANCO)
+		{
+			wait(100, milliseconds);
 
-			setMultipleMotors(-40,motorB,motorA);
-			wait(450,milliseconds);
-
+			setMultipleMotors(-40, motorB, motorA);
+			wait(450, milliseconds);
 
 			stopAllMotors();
 
-			while(cor1==BRANCO){ //VIRAR ATE ACHAR O PRETO
-				setMotor(motorA,-90);
-				setMotor(motorB,50);
-
+			while (cor1 == BRANCO)
+			{ //VIRAR ATE ACHAR O PRETO
+				setMotor(motorA, -90);
+				setMotor(motorB, 50);
 			}
 
 			stopAllMotors();
-			wait(200,milliseconds);
+			wait(200, milliseconds);
 
-			setMotor(motorA,25);
-			setMotor(motorB,-25);
-			wait(100,milliseconds);
-
+			setMotor(motorA, 25);
+			setMotor(motorB, -25);
+			wait(100, milliseconds);
 
 			stopAllMotors();
 
-		}//if cor4 do 90
+		} //if cor4 do 90
 
+	} //IF cor2 do 90
 
-	}//IF cor2 do 90
-
-}//VOID
+} //VOID
--- ixtark/codigos 3 sensores/noventagrausesquerda.h
+++ ixtark/codigos 3 sensores/noventagrausesquerda.h
@@ -1,34 +1,36 @@
-void noventagrausesquerda(){
+void noventagrausesquerda()
+{
 
 	//90 graus esquerda
-	if(cor1!=BRANCO){
+	if (cor1 != BRANCO)
+	{
 		stopAllMotors();
 		//wait(5000,milliseconds);
-		if(cor3!=BRANCO){
-			wait(100,milliseconds);
+		if (cor3 != BRANCO)
+		{
+			wait(100, milliseconds);
 
-			setMultipleMotors(-40,motorB,motorA);
-			wait(450,milliseconds);
-
+			setMultipleMotors(-40, motorB, motorA);
+			wait(450, milliseconds);
 
 			stopAllMotors();
 
-			while(cor2==BRANCO){ //VIRAR ATE ACHAR O PRETO
-				setMotor(motorA,50);
-				setMotor(motorB,-90);
-
+			while (cor2 == BRANCO)
+			{ //VIRAR ATE ACHAR O PRETO
+				setMotor(motorA, 50);
+				setMotor(motorB, -90);
 			}
 
 			stopAllMotors();
-			wait(200,milliseconds);
+			wait(200, milliseconds);
 
-			setMotor(motorA,-25);
-			setMotor(motorB,25);
-			wait(100,milliseconds);
+			setMotor(motorA, -25);
+			setMotor(motorB, 25);
+			wait(100, milliseconds);
 
 			stopAllMotors();
-		}//if do cor3 90
+		} //if do cor3 90
 
-	}//IF do cor1 branco
+	} //IF do cor1 branco
 
-}//VOID
+} //VOID
